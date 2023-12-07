df2020c1a51dbce9518863638ea3cfd00a4c456a
Kashish Shrivastava <132200555+kashish01-shrivastava@users.noreply.github.com>
2023-12-07 12:27:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import numpy as np
 import pickle
 import streamlit as st
 
-Diabetes_model=pickle.load(open("Diabetes_classifier.sav","rb"))
+Diabetes_model = pickle.load(open("Diabetes_classifier.sav", "rb"))
 
-#creating a function for prediction
+# Creating a function for prediction
 def diabetes_prediction(input_data):
     # changing input data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
@@ -21,30 +21,41 @@ def diabetes_prediction(input_data):
         return "you are diabetic"
 
 def main():
-
-    #title
+    # Title
     st.title("Predict Diabetes")
 
-    #getting input data from user
-    pregnancies=st.text_input("no. of pregnancies")
-    glucose=st.text_input("Enter Glucose level")
-    blood_pressure=st.text_input("Enter Blood Pressure")
-    skin_thickness=st.text_input("Enter Skin Thickness")
-    insulin=st.text_input("Enter Insulin")
-    BMI=st.text_input("Enter BMI")
-    diabetes_pedigree_function=st.text_input("Enter Diabetes Pedigree Function")
-    age=st.text_input("Enter Age")
+    # Getting input data from user
+    pregnancies = st.text_input("no. of pregnancies")
+    glucose = st.text_input("Enter Glucose level")
+    blood_pressure = st.text_input("Enter Blood Pressure")
+    skin_thickness = st.text_input("Enter Skin Thickness")
+    insulin = st.text_input("Enter Insulin")
+    BMI = st.text_input("Enter BMI")
+    diabetes_pedigree_function = st.text_input("Enter Diabetes Pedigree Function")
+    age = st.text_input("Enter Age")
 
-    #Enter code for Diagnosis
-    diagnosis=" "
+    # Enter code for Diagnosis
+    diagnosis = " "
 
-    #creating a button
+    # Creating a button
     if st.button("Diabetes Test Result"):
-        diagnosis=diabetes_prediction([pregnancies,glucose,blood_pressure, skin_thickness,insulin,BMI,diabetes_pedigree_function,age])
-
+        diagnosis = diabetes_prediction(
+            [pregnancies, glucose, blood_pressure, skin_thickness, insulin, BMI, diabetes_pedigree_function, age]
+        )
 
     st.success(diagnosis)
 
+    # Sidebar for additional pages
+    st.sidebar.title("Navigation")
+    selected_page = st.sidebar.radio("Select a page", ["Home", "About", "Contact Us"])
 
-if __name__ == '__main__':
+    if selected_page == "About":
+        st.write("This is a Streamlit app for predicting diabetes.")
+        # Add more about information here
+
+    elif selected_page == "Contact Us":
+        st.write("Contact us at example@email.com.")
+        # Add more contact information or a form here
+
+if __name__ == "__main__":
     main()
